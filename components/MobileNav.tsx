57fe8c25214d1dd00c1b30f7d203f46b6be64c84
Harshavardhan Patekar <66788476+hupatekar17@users.noreply.github.com>
2024-06-13 17:58:28 +0530

    sidebar implementation

---
+++ components/MobileNav.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from 'next/image'
+import { Link } from 'lucide-react'
+  
+
+const MobileNav = ({user}:MobileNavProps) => {
+  return (
+    <section className='w-full max-w-[264px] '>
+        <Sheet>
+  <SheetTrigger><Image src="/icons/hamburger.svg"
+  width={30}
+  height={30}
+  alt='menu'
+  className='cursor-pointer'/></SheetTrigger>
+  <SheetContent side="left">
+        <Link>
+        </Link>
+  </SheetContent>
+</Sheet>
+
+    </section>
+  )
+}
+
+export default MobileNav
